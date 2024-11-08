63f458acaf6b180a8844ea06801313f9a5fdf662
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-09 00:13:52 +0530

    install styled-components library and add css to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 import { NavLink } from "react-router-dom";
-
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/Navbar";
 function Navbar() {
   return (
-    <nav>
+    <Wrapper>
       <div className="nav-center">
-        <span className="logo">
-          <div className="nav-links">
-            <NavLink to="/" className="nav-link">
-              Home
-            </NavLink>
-            <NavLink to="/about" className="nav-link">
-              About
-            </NavLink>
-            <NavLink to="/newsletter" className="nav-link">
-              Newsletter
-            </NavLink>
-          </div>
-        </span>
+        <span className="logo">MixMaster</span>
+        <div className="nav-links">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+          <NavLink to="/newsletter" className="nav-link">
+            Newsletter
+          </NavLink>
+        </div>
       </div>
-    </nav>
+    </Wrapper>
   );
 }
 
