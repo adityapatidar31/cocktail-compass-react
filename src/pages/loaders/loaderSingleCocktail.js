4dbdc5b5d8637fc37d9e340b51997f326ac5fc1e
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-09 22:08:07 +0530

    add toast for different errors

--- src/pages/loaders/loaderSingleCocktail.js
+++ src/pages/loaders/loaderSingleCocktail.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+import { redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 export const loader = async ({ params }) => {
   const { id } = params;
-  const res = await axios.get(`${BASE_URL}${id}s`);
-  if (!res.data)
-    throw new Error(
-      "Drink is not found, please try to look for different drink"
-    );
-  return { id, drink: res.data.drinks[0] };
+  try {
+    const res = await axios.get(`${BASE_URL}${id}`);
+    if (!res.data) {
+      toast.error("Drink is not found");
+      return redirect("/");
+    }
+    return { id, drink: res.data.drinks[0] };
+  } catch {
+    toast.error("Internal error, Please try later");
+    return redirect("/");
+  }
 };
