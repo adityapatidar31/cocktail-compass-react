11f6ab518503f23479ba0bdf15db2a038edda118
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-09 22:17:12 +0530

    create a newsletter form

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,5 +1,52 @@
+import { Form } from "react-router-dom";
+
 function Newsletter() {
-  return <div>Newsletter</div>;
+  return (
+    <Form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          defaultValue="lucky"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          Last Name
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          defaultValue="patidar"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          className="form-input"
+          defaultValue="test@gmail.com"
+        />
+      </div>
+      <button className="btn btn-block" style={{ marginTop: "0.5rem" }}>
+        submit
+      </button>
+    </Form>
+  );
 }
 
 export default Newsletter;
