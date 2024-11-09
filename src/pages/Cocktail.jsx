b3816d9a1484ffee654faa18febf0ffcc980f6cf
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-09 21:48:23 +0530

    create a cocktail page completly with api loader and css

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,5 +1,66 @@
+import { Link, useLoaderData } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailPage";
+
 function Cocktail() {
-  return <div>Cocktail</div>;
+  const { drink } = useLoaderData();
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = drink;
+  const ingredientsKeys = Object.keys(drink).filter(
+    (key) => key.startsWith("strIngredient") && drink[key] != null
+  );
+  const ingredients = ingredientsKeys.map((ingredient) => drink[ingredient]);
+  console.log(ingredients);
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name:</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category:</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">Info:</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">Glass:</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">Instruction:</span>
+            {instructions}
+          </p>
+          <p>
+            <span className="drink-data">Ingredients:</span>
+            {ingredients.map((ingredient, idx) => {
+              return (
+                <span className="ing" key={ingredient}>
+                  {ingredient}
+                  {idx !== ingredients.length - 1 ? "," : "."}
+                </span>
+              );
+            })}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Cocktail;
--- src/pages/loaders/loaderSingleCocktail.js
+++ src/pages/loaders/loaderSingleCocktail.js
@@ -1,3 +1,9 @@
-export const loader = () => {
-  return "something";
+import axios from "axios";
+
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const res = await axios.get(`${BASE_URL}${id}`);
+  return { id, drink: res.data.drinks[0] };
 };
